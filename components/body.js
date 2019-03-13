4c722ee5dba0480f7ef69643945da547ab5ca388
Gaiish <gdishim@gmail.com>
2019-03-13 15:24:51 +0300

    added Flatlist

--- components/body.js
+++ components/body.js
@@ -1,7 +1,17 @@
 //body.js
+//Replacing the map function with the FlatList Component
+//it deals better with list and has more features
+//https://facebook.github.io/react-native/docs/flatlist
+
+//it a data prop where you put your list of notes and it will map through it
+//and then renderItem prop where you can define how you want the each element in the list
+//to look like; for us we will pass the Note Component
+//and we will use the keyExtractor prop also which will make sure that React can update
+//the specific Note needed to be updated
+
 import React, {Component} from 'react'
 import {
-  View, Text, StyleSheet, Image
+  View, Text, StyleSheet, Image, FlatList
 } from 'react-native'
 
 import Note from './note'
@@ -15,9 +25,12 @@ export default class Body extends Component {
     return(
       <View style={styles.list}>
         {this.props.notesList.length !==0 ?
-          this.props.notesList.map((note, i)=>(
-            <Note key={i} note={note} />
-          )) : (
+          (<FlatList
+            data = {this.props.notesList}
+            renderItem={({item})=><Note  note={item} />}
+            keyExtractor ={(item, index) => JSON.stringify(index)}
+          />)
+          : (
             <Image source={require('../img/no-note.png')} style={styles.img} />
           )
         }
